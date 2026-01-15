28e4fe50cebe79a9d57c66c5c02e915883ac1642
vahanm-dev <dev.vahanm@gmail.com>
2026-01-15 19:29:18 +0400

    added Root View

---
+++ Mercato/Core/Root/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Mercato
+//
+//  Created by Vahan Muradyan on 15.01.26.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RootView()
+}
