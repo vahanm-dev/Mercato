4d54e6595fdbb5179eb02bf0c7bf643507710290
vahanm-dev <dev.vahanm@gmail.com>
2026-01-15 19:20:19 +0400

    initial commit

---
+++ Mercato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mercato
+//
+//  Created by Vahan Muradyan on 15.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mercato/MercatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercatoApp.swift
+//  Mercato
+//
+//  Created by Vahan Muradyan on 15.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MercatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
